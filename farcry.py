6f484ad43b73e582b5e5d428237133d1f168a14a
Mai Bach <admin@Admins-MacBook-Air.local>
2019-05-12 22:21:39 +0700

    Declaring parse_session_mode_and_map function for WP4

--- farcry.py
+++ farcry.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from datetime import datetime
-from datetime import timezone
 from datetime import timedelta
 from datetime import timezone
 
@@ -13,19 +12,28 @@ def read_log_file(log_file_pathname):
 def parse_log_start_time(log_data):
 
 	data_list = log_data.split("\n")
-	raw_time = data_list[0][15:]
+	raw_time = data_list[0][15:].strip()
 	log_start_time = datetime.strptime(raw_time, "%A, %B %d, %Y %H:%M:%S")
 
 	for item in data_list:
 		if item.find("g_timezone") != -1:
-			time_delta = int(item.split('(')[1][:-1].split(",")[1])
+			time_delta = int(item.split('(')[1][:-1].split(",")[1].strip())
 			break
-			
+
 	timezone_info = timezone(timedelta(hours=time_delta))
 
 	return log_start_time.replace(tzinfo=timezone_info)
+
+
+def parse_session_mode_and_map(log_data):
+	data_list = log_data.split("\n")
+	for item in data_list:
+		if item.find("Loading level") != -1:
+			data_map = item.split(",")[0].split("/")[1].strip()
+			data_mode = item.split(",")[1].split("-")[0].strip().split(" ")[1].strip()
+			return (data_mode, data_map)
 		
 
 log_data = read_log_file("./logs/log00.txt")
-print(parse_log_start_time(log_data).isoformat())
+print(parse_session_mode_and_map(log_data))
 
