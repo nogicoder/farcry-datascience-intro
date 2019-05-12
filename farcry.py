e4d0a1babc2558f6a8df569ea09cdd577ea86ed0
Mai Bach <admin@Admins-MacBook-Air.local>
2019-05-12 21:17:47 +0700

    Declaring parse_log_start_time function for WP2

--- farcry.py
+++ farcry.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
+from datetime import datetime
+
+
 def read_log_file(log_file_pathname):
     with open(log_file_pathname, 'r') as file:
         return file.read()
 
 
+def parse_log_start_time(log_data):
+        log_start_time = log_data.split("\n")[0]
+        return datetime.strptime(log_start_time[15:], "%A, %B %d, %Y %H:%M:%S")
+
+
 log_data = read_log_file("./logs/log00.txt")
-print(len(log_data))
+print(parse_log_start_time(log_data).isoformat())
 
