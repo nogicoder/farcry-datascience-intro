534143a9beb474f734b916a9a72adef09cc946a8
Mai Bach <admin@Admins-MacBook-Air.local>
2019-05-12 22:56:07 +0700

    Declaring parse_frags for WP5

--- farcry.py
+++ farcry.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
+from re import findall
 
 
 def read_log_file(log_file_pathname):
@@ -15,9 +16,9 @@ def parse_log_start_time(log_data):
 	raw_time = data_list[0][15:].strip()
 	log_start_time = datetime.strptime(raw_time, "%A, %B %d, %Y %H:%M:%S")
 
-	for item in data_list:
-		if item.find("g_timezone") != -1:
-			time_delta = int(item.split('(')[1][:-1].split(",")[1].strip())
+	for line in data_list:
+		if line.find("g_timezone") != -1:
+			time_delta = int(line.split('(')[1][:-1].split(",")[1].strip())
 			break
 
 	timezone_info = timezone(timedelta(hours=time_delta))
@@ -27,13 +28,37 @@ def parse_log_start_time(log_data):
 
 def parse_session_mode_and_map(log_data):
 	data_list = log_data.split("\n")
-	for item in data_list:
-		if item.find("Loading level") != -1:
-			data_map = item.split(",")[0].split("/")[1].strip()
-			data_mode = item.split(",")[1].split("-")[0].strip().split(" ")[1].strip()
-			return (data_mode, data_map)
+	for line in data_list:
+		if line.find("Loading level") != -1:
+			line_map = line.split(",")[0].split("/")[1].strip()
+			line_mode = line.split(",")[1].split("-")[0].strip().split(" ")[1].strip()
+			return (line_mode, line_map)
+
+
+def parse_frags(log_data):
+
+	data_list = log_data.split("\n")
+	frags_list = []
+
+	for line in data_list:
+
+		if line.find("killed") != -1:
+			# <26:32> <Lua> papazark killed lamonthe with AG36
+			line_fragtime = line.split("<Lua>")[0].strip()[1:-1]
+			line_killer_name = line.split("<Lua>")[1].split("killed")[0].strip()
+			
+			if line.split("<Lua>")[1].split("killed")[1].strip().find("itself") == -1:
+				line_victim_name = line.split("<Lua>")[1].split(
+					"killed")[1].strip().split("with")[0].strip()
+				line_weapon_code = line.split("<Lua>")[1].split(
+					"killed")[1].strip().split("with")[1].strip()
+				frags_list.append((line_fragtime, line_killer_name, line_victim_name, line_weapon_code))
+			else:
+				frags_list.append((line_fragtime, line_killer_name))
+	
+	return frags_list
 		
 
 log_data = read_log_file("./logs/log00.txt")
-print(parse_session_mode_and_map(log_data))
+print(parse_frags(log_data))
 
