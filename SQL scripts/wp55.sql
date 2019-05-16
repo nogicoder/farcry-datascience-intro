ae0351092670d7d3149b97e1f7ed4c6c074445ed
Mai Bach <admin@Admins-MacBook-Air.local>
2019-05-17 03:39:57 +0700

    Refactoring farcry.py from WP1-25 and also WP55.sql

--- SQL scripts/wp55.sql
+++ SQL scripts/wp55.sql
@@ -11,23 +11,22 @@ DECLARE
     current_count INT;
     deltatime INT;
     i_fragtime TIMESTAMPTZ(3);
-    player_list TEXT[] := '{}'::TEXT[];
-    killer_name_ar TEXT[];
     c_line1 RECORD;
     c_line2 RECORD;
 BEGIN
     FOR c_line1 IN
     (SELECT DISTINCT T.match_id, T.killer_name
-    FROM match_frags T)
+    FROM match_frags T
+    ORDER BY T.match_id, T.killer_name)
     LOOP
+        match_id := c_line1.match_id;
         killer_name := c_line1.killer_name;
         i_fragtime := to_timestamp(0);
         max_count := 0;
         current_count := 0;
         FOR c_line2 IN (SELECT T.match_id, T.frag_time, T.killer_name, T.victim_name
                         FROM match_frags T
-                        WHERE T.killer_name = c_line1.killer_name OR T.victim_name = c_line1.killer_name
-                        ORDER BY T.frag_time)
+                        WHERE (T.killer_name = c_line1.killer_name) OR (T.victim_name = c_line1.killer_name))
             LOOP
                 deltatime = EXTRACT(EPOCH FROM(c_line2.frag_time - i_fragtime));
                 IF (i_fragtime = to_timestamp(0) OR
@@ -39,7 +38,7 @@ BEGIN
                     c_line2.victim_name = killer_name OR
                     (c_line2.killer_name = killer_name AND c_line2.victim_name IS NOT NULL AND deltatime > p_max_time_between_kills))
                 THEN 
-                    IF (current_count >= p_min_kill_count AND current_count >= max_count)
+                    IF (current_count >= p_min_kill_count AND current_count > max_count)
                     THEN 
                         max_count := current_count;
                     END IF;
@@ -49,9 +48,8 @@ BEGIN
                 END IF;
             END LOOP;
 
-        match_id := c_line1.match_id;
         kill_count := max_count;
         RETURN NEXT;
-END LOOP;
+    END LOOP;
 END; $$ 
 LANGUAGE PLPGSQL;
